eae2cd6cfad5ae46024a6d57f8956c421bed8575
Matt <matt@mspacecreative.com>
2021-06-30 16:58:45 -0300

    c

--- templates/blocks/inc/block-cta-text-size.php
+++ templates/blocks/inc/block-cta-text-size.php
@@ -3,11 +3,12 @@ $heading = get_field('heading');
 $headingsize = get_field('heading_size');
 $bodysize = get_field('paragraph_size');
 $content = get_field('content');
+$maxwidth = get_field('max_width');
 
 if ( $heading ): ?>
 <h1<?php if ( $headingsize == 'large' ): echo ' class="large"'; endif; ?>><?php echo $heading ?></h1>
 <?php endif;
 	
 if ( $content ): ?>
-<div class="cta-content<?php if ( $bodysize == 'large' ): echo ' large'; endif; ?>"><?php echo $content ?></div>
+<div class="cta-content<?php if ( $bodysize == 'large' ): echo ' large'; endif; ?>"<?php if ($maxwidth): echo ' style="max-width: '; echo $maxwidth; echo ';"'; endif; ?>><?php echo $content ?></div>
 <?php endif;
