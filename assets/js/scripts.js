7d66e3c22baf25a754e18150953242f923aace23
Matt <matt@mspacecreative.com>
2021-07-05 14:57:31 -0300

    c

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,7 +2,7 @@
 	"use strict";
 	
 	// MOVE BACKGROUND IMAGE HORIZONTALLY WHEN VISIBLE IN VIEWPORT
-	/*var bgimg = document.querySelector('.section_has_bg_img');
+	var bgimg = document.querySelector('.section_has_bg_img');
 	
 	(function() {
 		
@@ -28,19 +28,20 @@
 	})();
 	window.addEventListener("optimizedScroll", function() {
 		//leftItem.style.transform = "translateX(-" + window.pageYOffset / 5 + "px";
-		bgimg.style.background-position = "left" + window.pageYOffset / 5 + "px" + "center";
+		bgimg.style.backgroundPosition = "left" + window.pageYOffset / 5 + "px" + "center";
 	});
-	*/
 	
+	/*
 	$(window).on("load resize scroll", function() {
 	  $(".section_has_bg_img").each(function() {
 	    var windowTop = $(window).scrollTop();
 	    var elementTop = $(this).offset().top;
 	    var leftPosition = windowTop - elementTop;
 	      $(this)
-	        .css({ backgroundPosition: leftPosition + 'center' });
+	        .css({ backgroundPosition: leftPosition "center" });
 	  });
 	});
+	*/
 	
 	// SMOOTH SCROLL AFTER PAGE LOAD
 	if ( window.location.hash ) scroll(0,0);
