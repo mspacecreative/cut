7e3024af93edc44ef1ca8405ed1f6b7710f866be
Matt <matt@mspacecreative.com>
2021-07-05 14:30:14 -0300

    c

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -57,30 +57,28 @@
 		$.fn.visible = function(partial) {
     
 			var $t            = $(this),
-		          $w            = $(window),
-		          viewTop       = $w.scrollTop(),
-		          viewBottom    = viewTop + $w.height(),
-		          _top          = $t.offset().top,
-		          _bottom       = _top + $t.height(),
-		          compareTop    = partial === true ? _bottom : _top,
-		          compareBottom = partial === true ? _top : _bottom;
+		    	$w            = $(window),
+		        viewTop       = $w.scrollTop(),
+		        viewBottom    = viewTop + $w.height(),
+		        _top          = $t.offset().top,
+		        _bottom       = _top + $t.height(),
+		        compareTop    = partial === true ? _bottom : _top,
+		        compareBottom = partial === true ? _top : _bottom;
 		    
 		    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
 		
-		  };
-		    
-		})(jQuery);
+		};
 		
 		$(window).scroll(function(event) {
-		  
-		  $(".section_has_bg_img").each(function(i, el) {
-		    var el = $(el);
-		    if (el.visible(true)) {
-		      el.addClass("visible"); 
-		    } else {
-		      el.removeClass("visible");
-		    }
-		  });
+			$(".section_has_bg_img").each(function(i, el) {
+				var el = $(el);
+				if (el.visible(true)) {
+					el.addClass("visible");
+				} else {
+					el.removeClass("visible");
+				}
+			});
+		});
 	});
 	
 	// HIDE/SHOW HEADER ON SCROLL
