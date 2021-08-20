6f298b83ca359224bd6bea8863717d15e7af9d84
Matt <matt@mspacecreative.com>
2021-08-20 12:05:23 -0300

    c

--- functions/insight-cta.php
+++ functions/insight-cta.php
@@ -1,8 +1,8 @@
 <?php 
 function insight_cta() {
-	$external = get_field('external');
-	$internal = get_field('internal');
-	$download = get_field('download');
+	$external = get_field('external_link');
+	$internal = get_field('internal_page');
+	$download = get_field('file_upload');
 	$label = get_field('label');
 	
 	$html = '<a href="' . $external || $internal || $download . '">' . $label . '</a>';
