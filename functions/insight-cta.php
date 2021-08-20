b75596770089fdc86c285c3fbccf890f0517b2f0
Matt <matt@mspacecreative.com>
2021-08-20 12:14:23 -0300

    c

--- functions/insight-cta.php
+++ functions/insight-cta.php
@@ -4,7 +4,15 @@ function insight_cta() {
 	$internal = get_field('internal_page', get_the_ID());
 	$download = get_field('file_upload', get_the_ID());
 	$label = get_field('label', get_the_ID());
+	$html = '';
 	
-	$html = '<a href="' . $external || $internal || $download . '">' . $label . '</a>';
+	if ($external) {
+		$html = '<a href="' . $external . '" target="_blank">' . $label . '</a>';
+	} elseif ($internal) {
+		$html = '<a href="' . $external . '">' . $label . '</a>';
+	} elseif ($download) {
+		$html = '<a href="' . $download . '" target="_blank">' . $label . '</a>';
+	}
+	
 	echo $html;
 }
