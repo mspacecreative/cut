df73dd7961709ef123b88f9e62d29d68494b4fbc
Matt <matt@mspacecreative.com>
2021-08-20 13:44:31 -0300

    c

--- functions/insight-cta.php
+++ functions/insight-cta.php
@@ -3,15 +3,21 @@ function insight_cta() {
 	$external = get_field('external_link', get_the_ID());
 	$internal = get_field('internal_page', get_the_ID());
 	$download = get_field('file_upload', get_the_ID());
-	$label = get_field('label', get_the_ID());
+	$customlabel = get_field('label', get_the_ID());
 	$html = '';
 	
 	if ($external) {
-		$html = '<p style="align-self: flex-start;"><a class="button solid" href="' . $external . '" target="_blank">' . $label . '</a></p>';
+		$html = '<p style="align-self: flex-start;"><a class="button solid" href="' . $external . '" target="_blank">';
+		 			if ($customlabel) {
+		 				$customlabel;
+		 			} else {
+		 				__('Learn more'); 
+		 			}
+		 		'</a></p>';
 	} elseif ($internal) {
-		$html = '<p style="align-self: flex-start;"><a class="button solid" href="' . $external . '">' . $label . '</a></p>';
+		$html = '<p style="align-self: flex-start;"><a class="button solid" href="' . $external . '">' . $customlabel . '</a></p>';
 	} elseif ($download) {
-		$html = '<p style="align-self: flex-start;"><a class="button solid" href="' . $download . '" target="_blank">' . $label . '</a></p>';
+		$html = '<p style="align-self: flex-start;"><a class="button solid" href="' . $download . '" target="_blank">' . $customlabel . '</a></p>';
 	}
 	
 	echo $html;
